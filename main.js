55f37b9bf3f516cdf9caf1e5b0211dd775f369fc
Gusarich <danielsedovzzz@gmail.com>
2022-02-06 16:19:13 +0300

    website

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', async function () {
+    if (typeof window.ethereum !== 'undefined') {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        if (account) {
+            document.getElementsByClassName('metamask')[0].innerText = account.substring(0, 4) + '...' + account.slice(-4);
+        }
+    }
+})
