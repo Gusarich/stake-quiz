aef0063b3f73b75d90d4fde3f858c1f5f6f65ea1
Gusarich <danielsedovzzz@gmail.com>
2022-02-06 16:29:50 +0300

    update

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ window.addEventListener('load', async function () {
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
         if (account) {
-            document.getElementsByClassName('metamask')[0].innerText = account.substring(0, 4) + '...' + account.slice(-4);
+            document.getElementsByClassName('metamask-text')[0].innerText = account.substring(0, 4) + '...' + account.slice(-4);
+            document.getElementsByClassName('signout')[0].style.visibility = 'visible';
         }
     }
 })
